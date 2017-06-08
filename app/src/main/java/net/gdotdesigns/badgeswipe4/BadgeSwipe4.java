48095c7cd11a10fbe50f710f484b9f12227beb58
gdotdesigns <gdotdesigns@gmail.com>
2017-06-08 16:26:36 -0500

    Initial commit

---
+++ app/src/main/java/net/gdotdesigns/badgeswipe4/BadgeSwipe4.java
@@ -0,0 +1,13 @@
+package net.gdotdesigns.badgeswipe4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BadgeSwipe4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badge_swipe4);
+    }
+}
