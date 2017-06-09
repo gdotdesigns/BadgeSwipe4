e2af76befcb0af666b1b6c93ba76fb0295df806c
gdotdesigns <gdotdesigns@gmail.com>
2017-06-09 10:34:05 -0500

    Building framework.

--- app/src/main/java/net/gdotdesigns/badgeswipe4/BadgeSwipe4.java
+++ app/src/main/java/net/gdotdesigns/badgeswipe4/BadgeSwipe4.java
@@ -1,9 +1,17 @@
 package net.gdotdesigns.badgeswipe4;
 
+import android.icu.text.DateFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class BadgeSwipe4 extends AppCompatActivity {
+
+    TextView timeStamp = (TextView) findViewById(R.id.timeStamp);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +19,13 @@ public class BadgeSwipe4 extends AppCompatActivity {
         setContentView(R.layout.activity_badge_swipe4);
     }
 
+
+    public void badgeSwiped(View view){
+
+        Date currentDateTimeString = Calendar.getInstance().getTime();
+
+        timeStamp.setText("");
+
+    }
 
 }
